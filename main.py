281d131cfc1a887945b7ce32be4b7d2a7cac06de
BelugaWhaleSam <sameermohd2k1@gmail.com>
2023-11-11 23:57:22 +0530

    final commit-spin function

--- main.py
+++ main.py
@@ -110,8 +110,7 @@ def get_bet():
     
     return amount
 
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     
     while True:
@@ -129,5 +128,17 @@ def main():
     winnings, winning_lines = check_winnings(slots,lines,bet,symbol_values)
     print(f"You won ${winnings}.")
     print(f"You won on lines:", *winning_lines)
+    return winnings-total_bet
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is${balance}")
+        answer = input("Press enter to spin(q to quit)")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"Your final balance is ${balance}")
 
 main()
